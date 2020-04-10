b8ff6361040b5b78eed7991e2de0db8209f7d9f3
andrew-savich <savichandrew@Gmail.com>
2020-04-10 17:13:15 +0300

    Starting a project

---
+++ TravelTelegramBot/src/main/java/com/andrewsavich/traveltelegrambot/TravelTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.andrewsavich.traveltelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelTelegramBotApplication.class, args);
+	}
+
+}
---
+++ TravelTelegramBot/src/test/java/com/andrewsavich/traveltelegrambot/TravelTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrewsavich.traveltelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
